a7813c2c2d638c509af1b24504dbed64ea23d703
serhiialpatov <alpatovsn@gmail.com>
2022-10-16 14:25:34 +0300

    + __set_deck shuffled_deck get_card_value

--- service.py
+++ service.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Deck:
     def __init__(self):
         denomination = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
@@ -9,8 +12,14 @@ class Deck:
             for y in denomination:
                 deck.update({f"{y}{x}": values[itt]})
                 itt += 1
-        self.deck = deck
-    def set_deck(self):
-        return list(self.deck.keys())
+        self.__deck = deck
 
+    def __set_deck(self):
+        return list(self.__deck.keys())
+
+    def shuffled_deck(self):
+        return random.shuffle(self.__set_deck())
+
+    def get_card_value(self, card):
+        return self.__deck.get(card, 0)
 
